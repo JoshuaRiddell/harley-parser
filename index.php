f77f64eb27c24f83b2ea773b008772e1b2c7583d
Joshua Riddell <joshua.riddell11@gmail.com>
2015-12-17 16:23:32 +1000

    gh-pages php test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<?php include './php/head.php' ;?>
+
+<textarea autofocus id="tree_input"></textarea>
+
+<textarea id="aiml_output"></textarea>
+
+<script src="./js/parser"></script>
+
+</html>
